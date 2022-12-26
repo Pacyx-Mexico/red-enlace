c433220961feca132b13d67203b6ca43a89f65e0
Doniben <doniben@esperanto.co>
2022-12-26 12:50:44 -0500

    Description mobile

--- src/components/gastos-medicos/Description.jsx
+++ src/components/gastos-medicos/Description.jsx
@@ -1,18 +1,19 @@
 import styled from "styled-components";
 import descriptionImg from "../../assets/images/description-image.png"
+import descriptionImg_mb from "../../assets/images/description-image.png";
 
 export const Description = () => {
   return (
     <DescriptionSection className="container-fluid">
-      <div className="d-flex flex-column">
+      <div className="d-flex flex-column d-none d-md-block">
         <div className="row mt-5 pt-5">
           <div className="col-md-5">
-            <img className="img-fluid h-100" src={descriptionImg} alt="" />
+            <img className="img-fluid h-100 position-relative" src={descriptionImg} alt="" />
           </div>
           <div className="col-md-7 col-xl-7 d-flex flex-column justify-content-center align-items-start text-start text-lg-start ml-5 pl-5">
             <div className="d-flex flex-column mx-auto">
               <h1 className="fw-normal mt-5 pt-5 text-start">Devolución</h1>
-              <h1 className="fw-bold">de deducible</h1>
+              <h1 className="fw-bold text-start">de deducible</h1>
               <p className="text-secondary mt-2 mb-5 fs-2 fw-light">
                 Es una asistencia que en caso de que te <br />hospitalicen, te reembolsa un porcentaje fijo del <br />deducible  de tu  póliza de gastos médicos mayores. <br />
               </p>
@@ -27,16 +28,62 @@ export const Description = () => {
         </div>
         <hr className="border border-5 border-lile my-0"/>
       </div>
+
+      <div className="d-flex flex-column d-md-none">
+        <div className="row mt-5 pt-5">
+          <div className="col-md-7 col-xl-7 d-flex flex-column justify-content-center align-items-start text-start text-lg-start ml-5 pl-5">
+            <div className="d-flex flex-column mx-auto">
+              <h1 className="fw-normal mt-5 pt-5 fs-42">Devolución</h1>
+              <h1 className="fw-bold">de deducible</h1>
+              <p className="mt-5 pt-5 mb-5 fs-15 text-center">
+                Es una asistencia que en caso de que<br />te hospitalicen, te reembolsa un<br />porcentaje fijo del deducible  de tu<br />póliza de gastos médicos mayores.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-5">
+            <img className="img-fluid h-100 position-relative" src={descriptionImg_mb} alt="" />
+          </div>
+          <div className="px-4">
+            <p className="mt-3 fs-20 fw-light text-center">
+              Con nosotros, tú eliges el porcentaje:
+            </p>
+            <p className="text-dark mb-5 fs-30 fw-bold text-center">
+              30, 50, 80 o hasta 100%
+            </p>
+          </div>
+        </div>
+        <hr className="border border-5 border-lile my-0"/>
+      </div>
     </DescriptionSection>
   )
 }
 
 const DescriptionSection = styled.section`
-  height: 92rem;
+  height: 100%;
 
   .text-lile,
   .border-lile {
   color: #6983d6;
   border: 10px solid #6983d6 !important;
+  }
+
+  .col-md-5 .position-relative {
+    left: -10rem;
+  }
+
+  .fs-30 {
+    font-size: 30px;
+  }
+
+  .fs-20 {
+    font-size: 20px;
+  }
+
+  .fs-15 {
+    font-size: 15px;
+  }
+
+  .fs-42 {
+    font-size: 42px;
   }
 `
