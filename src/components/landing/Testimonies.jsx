237f75976cf095e68fdae33cf76e69aab8ee3728
Doniben <doniben@esperanto.co>
2022-12-26 11:39:07 -0500

    Testimonies mobile version

--- src/components/landing/Testimonies.jsx
+++ src/components/landing/Testimonies.jsx
@@ -27,7 +27,7 @@ export const Testimonies = () => {
   ];
   return (
     <TestimonialsSection>
-      <div className="container position-relative">
+      <div className="container position-relative d-none d-md-block">
         <div className="testimonials__title my-4">
           <h2 className="text-center fw-normal lh-sm text-dark">
             Testimoniales
@@ -66,9 +66,9 @@ export const Testimonies = () => {
           {
             <SwiperSlide>
               <div className="testimonials__carousel-bg card border-none shadow position-relative text-dark p-3 pt-xxl-5 px-md-5 mx-5 my-5">
-                <div className="testimonials__user-info my-4 my-lg-5 py-4 d-flex flex-column px-md-3 px-xl-5 gap-xl-5 overflow-auto">
-                  <div className="flex-column px-sm-4 align-items-start justify-content-start flex-shrink-0 overflow-auto">
-                    <div className="text-start text-lg-start ms-lg-3">
+                <div className="testimonials__user-info my-4 my-lg-5 py-4 d-flex flex-column px-md-3 px-xl-5 gap-xl-5">
+                  <div className="flex-column align-items-start justify-content-start flex-shrink-0">
+                    <div className="text-start text-lg-start">
                       <h5 className="mb-1 text-start">
                         Carla
                         <span className="d-block fs-1">CDMX</span>
@@ -76,7 +76,7 @@ export const Testimonies = () => {
                     </div>
                   </div>
                   <div className="mt-4 mt-md-0 mb-3 mb-md-0 px-sm-1 px-xl-2">
-                    <p className="text-center text-sm-start mb-0 px-xxl-4 fs-2 lh-1">
+                    <p className="text-center text-sm-start mb-0 fs-2 lh-1">
                       Con Red Enlace pude renovar mi póliza de gastos médicos a
                       menor costo.
                     </p>
@@ -87,9 +87,9 @@ export const Testimonies = () => {
           }
           <SwiperSlide>
             <div className="testimonials__carousel-bg card border-none shadow position-relative text-dark p-3 pt-xxl-5 px-md-5 mx-5 my-5">
-              <div className="testimonials__user-info my-4 my-lg-5 py-4 d-flex flex-column px-md-3 px-xl-5 gap-xl-5 overflow-auto">
-                <div className="flex-column px-sm-4 align-items-start justify-content-start flex-shrink-0 overflow-auto">
-                  <div className="text-start text-lg-start ms-lg-3">
+              <div className="testimonials__user-info my-4 my-lg-5 py-4 d-flex flex-column px-md-3 px-xl-5 gap-xl-5">
+                <div className="flex-column align-items-start justify-content-start flex-shrink-0">
+                  <div className="text-start text-lg-start">
                     <h5 className="mb-1 text-start">
                       Raúl
                       <span className="d-block fs-1">Guadalajara</span>
@@ -97,7 +97,7 @@ export const Testimonies = () => {
                   </div>
                 </div>
                 <div className="mt-4 mt-md-0 mb-3 mb-md-0 px-sm-1 px-xl-2">
-                  <p className="text-center text-sm-start mb-0 px-xxl-4 fs-2 lh-1">
+                  <p className="text-center text-sm-start mb-0 fs-2 lh-1">
                     Cuando lo necesité estuvieron siempre al pendiente de mi
                     siniestro.
                   </p>
@@ -107,9 +107,9 @@ export const Testimonies = () => {
           </SwiperSlide>
           <SwiperSlide>
             <div className="testimonials__carousel-bg card border-none shadow position-relative text-dark p-3 pt-xxl-5 px-md-5 mx-5 my-5">
-              <div className="testimonials__user-info my-4 my-lg-5 py-4 d-flex flex-column px-md-3 px-xl-5 gap-xl-5 overflow-auto">
-                <div className="flex-column px-sm-4 align-items-start justify-content-start flex-shrink-0 overflow-auto">
-                  <div className="text-start text-lg-start ms-lg-3">
+              <div className="testimonials__user-info my-4 my-lg-5 py-4 d-flex flex-column px-md-3 px-xl-5 gap-xl-5">
+                <div className="flex-column align-items-start justify-content-start flex-shrink-0">
+                  <div className="text-start text-lg-start">
                     <h5 className="mb-1 text-start">
                       Diana
                       <span className="d-block fs-1">California</span>
@@ -117,7 +117,7 @@ export const Testimonies = () => {
                   </div>
                 </div>
                 <div className="mt-4 mt-md-0 mb-3 mb-md-0 px-sm-1 px-xl-2">
-                  <p className="text-center text-sm-start mb-0 px-xxl-4 fs-2 lh-1">
+                  <p className="text-center text-sm-start mb-0 fs-2 lh-1">
                     Me gusta la facilidad para cotizar y contratar.
                   </p>
                 </div>
@@ -125,6 +125,86 @@ export const Testimonies = () => {
             </div>
           </SwiperSlide>
         </Swiper>
+      </div>
+
+      <div className="container position-relative d-md-none">
+        <div className="testimonials__title my-4">
+          <h2 className="text-center fw-normal lh-sm text-dark">
+            Testimoniales
+          </h2>
+          <div className="d-flex flex-row justify-content-center">
+            <div className="m-4">
+              <StarIcon />
+            </div>
+            <div className="m-4">
+              <StarIcon />
+            </div>
+            <div className="m-4">
+              <StarIcon />
+            </div>
+            <div className="m-4">
+              <StarIcon />
+            </div>
+            <div className="m-4">
+              <StarIcon />
+            </div>
+          </div>
+        </div>
+        
+        <div className="testimonials__carousel-bg card border-none shadow-lg position-relative text-dark p-3 mx-5 my-5">
+          <div className="testimonials__user-info my-4 py-4 d-flex flex-column px-5">
+            <div className="flex-column ps-1 align-items-start justify-content-start flex-shrink-0">
+              <div className="text-start">
+                <h5 className="mb-1 text-start fw-semibold">
+                  Diana
+                  <span className="ms-5 fs-1 fst-italic fw-medium">California</span>
+                </h5>
+              </div>
+            </div>
+            <div className="mt-4 ps-1">
+              <p className="mb-0 fs-2 lh-1">
+                Me gusta la facilidad para cotizar y< br></br>contratar.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="testimonials__carousel-bg card border-none shadow-lg position-relative text-dark p-3 mx-5 my-5">
+          <div className="testimonials__user-info my-4 py-4 d-flex flex-column px-5">
+            <div className="flex-column ps-1 align-items-start justify-content-start flex-shrink-0">
+              <div className="text-start">
+                <h5 className="mb-1 text-start fw-semibold">
+                  Diana
+                  <span className="ms-5 fs-1 fst-italic fw-medium">California</span>
+                </h5>
+              </div>
+            </div>
+            <div className="mt-4 ps-1">
+              <p className="mb-0 fs-2 lh-1">
+                Me gusta la facilidad para cotizar y< br></br>contratar.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="testimonials__carousel-bg card border-none shadow-lg position-relative text-dark p-3 mx-5 my-5">
+          <div className="testimonials__user-info my-4 py-4 d-flex flex-column px-5">
+            <div className="flex-column ps-1 align-items-start justify-content-start flex-shrink-0">
+              <div className="text-start">
+                <h5 className="mb-1 text-start fw-semibold">
+                  Diana
+                  <span className="ms-5 fs-1 fst-italic fw-medium">California</span>
+                </h5>
+              </div>
+            </div>
+            <div className="mt-4 ps-1">
+              <p className="mb-0 fs-2 lh-1">
+                Me gusta la facilidad para cotizar y< br></br>contratar.
+              </p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </TestimonialsSection>
   );
@@ -151,5 +231,11 @@ const TestimonialsSection = styled.section`
     font-size: 1.125rem;
     font-weight: 400;
     opacity: 0.85;
+  }
+
+  @media (max-width: 768px) {
+    .testimonials__carousel-bg {
+      border-radius: 4rem;
+    }
   }
 `;
