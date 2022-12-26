4b660bc7296ab3499a00b6b6571c691484e54446
Doniben <doniben@esperanto.co>
2022-12-26 11:45:17 -0500

    header video updated

--- src/components/productos/HeaderVideo.jsx
+++ src/components/productos/HeaderVideo.jsx
@@ -10,26 +10,6 @@ function HeaderVideo({ video, title }) {
 
   return (
     <>
-      {/* <section className="headerText">
-        <h2 className="headerTitle__title">
-          Devolución de deducible <span>{title}</span>
-        </h2>
-        <h5 className="headerTitle__subtitle">
-          Para que la única recuperación por la que te preocupes{" "}
-          <span>sea la tuya.</span>
-        </h5>
-        <div onClick={scrollTop}>
-          <Link
-            to="/cotizador-deducible-gastos-medicos"
-            className="headerTitle__btn"
-          >
-            <Button text="Contratar" />
-          </Link>
-        </div>
-        <div className="headerText__imgContainer">
-        </div>
-      </section> */}
-
       {video ? (
         <section>
           <h4 className="videoTitle mt-5 pt-5">¿Cómo funciona?</h4>
@@ -41,13 +21,13 @@ function HeaderVideo({ video, title }) {
             </h5>
             <div onClick={scrollTop} className="d-none d-md-block">
               <Link to="/cotizador-deducible-gastos-medicos">
-                <Button text="Contratar"/>
+                <Button text="Contratar" />
               </Link>
             </div>
 
             <div onClick={scrollTop} className="d-md-none">
               <Link to="/cotizador-deducible-gastos-medicos">
-                <Button text="Contratar"/>
+                <Button text="Contratar" />
               </Link>
             </div>
           </div>
