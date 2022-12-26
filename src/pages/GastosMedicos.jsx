d8e458465eb6563ca4b117855937c400f73c4e05
Doniben <doniben@esperanto.co>
2022-12-26 11:46:39 -0500

    Gastos médicos page

--- src/pages/GastosMedicos.jsx
+++ src/pages/GastosMedicos.jsx
@@ -1,12 +1,16 @@
 import Navbar from "../components/Navbar";
 import HeaderVideo from "../components/productos/HeaderVideo";
+import { GastosMedicosHero, Description, Toppings } from '../components/gastos-medicos';
 import Footer from "../components/Footer";
 
 export default function GastosMedicos() {
   return (
     <>
       <Navbar type="productos" />
+      <GastosMedicosHero />
       <HeaderVideo video={true} title="Gastos Médicos" />
+      <Description />
+      <Toppings />
       <Footer />
     </>
   );
