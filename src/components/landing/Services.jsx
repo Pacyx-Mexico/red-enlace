e69f3f88284501014bab4f0cb84e2f0ab88891ac
Doniben <doniben@esperanto.co>
2022-12-27 15:55:38 -0500

    New services on home

--- src/components/landing/Services.jsx
+++ src/components/landing/Services.jsx
@@ -55,79 +55,50 @@ export const Services = () => {
         </h3>
       </div>
       <div className="container-fluid py-3 position-relative d-none d-md-block">
-        <div className="row px-5 mx-5 mr-5 ml-5 d-flex justify-content-between">
+        <div className="row px-5 mx-5 mr-5 ml-5 d-flex justify-content-around gap-5">
           <div className="col-auto mb-5 pb-5">
-            <div className="card border d-flex justify-content-center align-items-center border-4 border-first rounded-5 py-5 px-5 text-center opacity-75 ">
-              <div className="icon-width">
-                <MedicineIcon />
+            <Link onClick={ scrollTop } to="/deducible-gastos-medicos" className="w-100 text-decoration-none">
+              <div className="card border border-4 border-first rounded-5 text-center opacity-75 position-relative card-hover_first">
+                <div className="icon-width position-absolute d-flex align-items-center justify-content-center">
+                  <img src="https://elementos-red-enlace.s3.amazonaws.com/Landing/hombre-gastos-medicos.png" alt="Gastos medicos hombre" className="card_img-width card_img-width_first position-relative bottom-0 mx-auto"/>
+                </div>
               </div>
-              <div className="card-body px-2 pb-0">
-                <p className="card-title fw-normal lh-sm text-first fs-2">
-                  Devolución de <br />
-                  deducible Gastos
-                  <br /> Médicos
-                </p>
-              </div>
-            </div>
-            <div className="mt-5 d-flex w-100">
-              <Link onClick={ scrollTop } to="/deducible-gastos-medicos" className="w-100">
-                <button className="btn btn-lg btn-first rounded-pill fw-bold mx-auto w-100 text-white opacity-75 fs-1">
-                  Cotizar
-                </button>
-              </Link>
-            </div>
-          </div>
-          <div className="col-auto">
-            <div className="card border border-4 border-second rounded-5 py-5 px-5 text-center opacity-75 d-flex justify-content-center align-items-center">
-              <div className="icon-width">
-                <CarIcon />
-              </div>
-              <div className="card-body px-2 pb-0">
-                <p className="card-title fw-normal lh-sm text-second fs-2 mt-5 pt-2">
-                  Devolución de <br />
-                  deducible Autos
-                </p>
-              </div>
-            </div>
-            <div className="mt-5 d-flex w-100">
-              <button className="btn btn-lg btn-second rounded-pill fw-bold mx-auto w-100 text-white opacity-75 fs-1">
-                Cotizar
-              </button>
-            </div>
-          </div>
-          <div className="col-auto">
-            <div className="card border border-4 border-third rounded-5 py-5 px-5 text-center opacity-75 d-flex justify-content-center align-items-center">
-              <div className="w-100">
-                <UserPolicyMontain />
-              </div>
-              <div className="card-body px-2 pb-0">
-                <p className="card-title fw-normal lh-sm text-third fs-2">
-                  Al fin seguro
-                </p>
-              </div>
-            </div>
-            <div className="mt-5 d-flex w-100">
-              <button className="btn btn-lg btn-third rounded-pill fw-bold mx-auto w-100 text-white opacity-75 fs-1">
-                Cotizar
-              </button>
-            </div>
-          </div>
-          <div className="col-auto">
-            <div className="card border border-4 border-fourth rounded-5 py-5 px-5 text-center opacity-75 d-flex justify-content-center align-items-center">
-              <div className="icon-width">
-                <MedicalExpensesIcon />
-              </div>
-              <div className="card-body px-2 pb-0">
-                <p className="card-title fw-normal lh-sm text-fourth fs-2">
+              <div className="mt-5 d-flex justify-content-center w-100">
+                <p className="card-title fw-normal lh-sm text-center fs-2">
+                  Devolución de deducible<br />
                   Gastos Médicos
                 </p>
               </div>
-            </div>
-            <div className="mt-5 d-flex w-100">
-              <button className="btn btn-lg btn-fourth rounded-pill fw-bold mx-auto w-100 text-white opacity-75 fs-1">
-                Cotizar
-              </button>
-            </div>
+            </Link>
+          </div>
+          <div className="col-auto mb-5 pb-5">
+            <Link onClick={ scrollTop } to="/deducible-gastos-medicos" className="w-100 text-decoration-none">
+              <div className="card border border-4 border-second rounded-5 text-center opacity-75 position-relative card-hover_second">
+                <div className="icon-width position-absolute d-flex align-items-center justify-content-center">
+                  <img src="https://elementos-red-enlace.s3.amazonaws.com/Landing/hombre-autos.png" alt="hombre auto" className="card_img-width card_img-width_second position-relative bottom-0 mx-auto"/>
+                </div>
+              </div>
+              <div className="mt-5 d-flex justify-content-center w-100">
+                <p className="card-title fw-normal lh-sm text-center fs-2">
+                  Devolución de deducible<br />
+                  Autos
+                </p>
+              </div>
+            </Link>
+          </div>
+          <div className="col-auto mb-5 pb-5">
+            <Link onClick={ scrollTop } to="/deducible-gastos-medicos" className="w-100 text-decoration-none">
+              <div className="card border border-4 border-fourth rounded-5 text-center opacity-75 position-relative card-hover_third">
+                <div className="icon-width position-absolute d-flex align-items-center justify-content-center">
+                  <img src="https://elementos-red-enlace.s3.amazonaws.com/Landing/mujer-enlaces.png" alt="hombre auto" className="card_img-width card_img-width_third position-relative bottom-0 mx-auto"/>
+                </div>
+              </div>
+              <div className="mt-5 d-flex justify-content-center w-100">
+                <p className="card-title fw-normal lh-sm text-center fs-2">
+                  Enlaces
+                </p>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
@@ -136,7 +107,7 @@ export const Services = () => {
         <div className="row px-5 mx-5 mr-5 ml-5 d-flex justify-content-between">
           <div className="d-flex justify-content-center gap-10 px-5">
             <div className="col-auto mb-5 pb-5">
-              <div className="card border d-flex justify-content-center align-items-center border-4 border-first rounded-5 py-5 px-5 text-center opacity-75 ">
+              <div className="card border d-flex justify-content-center align-items-center border-4 border-second rounded-5 py-5 px-5 text-center opacity-75">
                 <div className="icon-width">
                   <MedicineIcon />
                 </div>
@@ -178,23 +149,6 @@ export const Services = () => {
 
           <div className="d-flex justify-content-center gap-10 px-5">
             <div className="col-auto">
-              <div className="card border border-4 border-third rounded-5 py-5 px-5 text-center opacity-75 d-flex justify-content-center align-items-center">
-                <div className="w-100">
-                  <UserPolicyMontain />
-                </div>
-                <div className="card-body px-2 pb-0">
-                  <p className="card-title fw-normal lh-sm text-third fs-2">
-                    Al fin seguro
-                  </p>
-                </div>
-              </div>
-              <div className="mt-5 d-flex w-100">
-                <button className="btn btn-lg btn-third rounded-pill fw-bold mx-auto w-100 text-white opacity-75 fs-1">
-                  Cotizar
-                </button>
-              </div>
-            </div>
-            <div className="col-auto">
               <div className="card border border-4 border-fourth rounded-5 py-5 px-5 text-center opacity-75 d-flex justify-content-center align-items-center">
                 <div className="icon-width">
                   <MedicalExpensesIcon />
@@ -233,6 +187,7 @@ const ServicesContainer = styled.section`
   .card-title {
     margin-bottom: 0rem;
     width: 25.95rem;
+    color: #000000;
   }
 
   .text-,
@@ -263,12 +218,48 @@ const ServicesContainer = styled.section`
   }
 
   .card {
-    width: 28rem;
-    height: 25rem;
+    width: 35rem;
+    height: 45rem;
 
-    .icon-width {
-      width: 12rem;
+    .card_img-width {
+      width: 35.2rem;
     }
+  }
+
+  .card_img-width_first {
+    top: -7.8rem;
+    right: 1rem;
+
+  }
+
+  .card_img-width_second {
+    top: -1.8rem;
+    right: 0.8rem;
+  }
+
+  .card_img-width_third {
+    width: 37.2rem !important;
+    top: -7.3rem;
+    right: 4rem;
+  }
+
+  .card-hover_first:hover {
+    box-shadow: 0 0 15px #5674d1;
+    transition: box-shadow .2s;
+  }
+
+  .card-hover_second:hover {
+    box-shadow: 0 0 15px #538fa8;
+    transition: box-shadow .2s;
+  }
+
+  .card-hover_third:hover {
+    box-shadow: 0 0 15px #1da939;
+    transition: box-shadow .2s;
+  }
+
+  .rounded-5 {
+    border-radius: 5.5rem !important;
   }
 
   .btn-first {
