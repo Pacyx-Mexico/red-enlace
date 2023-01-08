83c4eb8d8c6b17398274f12b4d0fc4826a1a1a15
Doniben <doniben@esperanto.co>
2023-01-08 14:36:26 -0500

    Fixed Hovers

--- src/components/landing/NewCharacteristics.jsx
+++ src/components/landing/NewCharacteristics.jsx
@@ -1,60 +1,126 @@
 import styled from "styled-components";
+import { useState } from "react";
 import { Image } from "react-bootstrap";
-import {
-  PersonIcon,
-  ReportIcon,
-  ChatIcon,
-  IdeaIcon,
-} from "../../assets/icons";
+import { PersonIcon, ReportIcon, ChatIcon, IdeaIcon } from "../../assets/icons";
+import Fade from 'react-bootstrap/Fade';
 // import { GreenStain } from "../../assets/backgrounds";
 
 export const NewCharacteristics = () => {
+  const [isHovering1, setIsHovering1] = useState(false);
+  const [isHovering2, setIsHovering2] = useState(false);
+  const [isHovering3, setIsHovering3] = useState(false);
+  const [isHovering4, setIsHovering4] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering1(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering1(false);
+  };
+
+  const handleMouseOver2 = () => {
+    setIsHovering2(true);
+  };
+
+  const handleMouseOut2 = () => {
+    setIsHovering2(false);
+  };
+
+  const handleMouseOver3 = () => {
+    setIsHovering3(true);
+  };
+
+  const handleMouseOut3 = () => {
+    setIsHovering3(false);
+  };
+
+  const handleMouseOver4 = () => {
+    setIsHovering4(true);
+  };
+
+  const handleMouseOut4 = () => {
+    setIsHovering4(false);
+  };
   return (
     <CharacteristicsSection className="position-relative">
       <div className="services-title container text-center mb-5 d-none d-md-block">
         <h3 className="pb-2 fw-semi-bold text-dark">
-          <span className="text-dark fw-normal">Complementamos<br/> <span className="text-dark fw-bold bold-fs">tu protección</span></span>
+          <span className="text-dark fw-normal">
+            Complementamos
+            <br />{" "}
+            <span className="text-dark fw-bold bold-fs">tu protección</span>
+          </span>
         </h3>
       </div>
       <div className="mt-5 pt-5 w-100 d-none d-md-block">
         <div className="services-position">
           <ul className="d-flex flex-row justify-content-around mx-auto">
             <li>
-              <p className="services__title">
+              <p
+                className="services__title"
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+              >
                 Procesos eficientes
               </p>
-              <p className="services-hover text-center">
-              El proceso para contratar cualquiera de <br />
-              nuestros productos es fácil y sencillo.
-              </p>
+              {isHovering1 && (
+                <Fade in={isHovering1} >
+                  <p className="fs-2 text-center">
+                    El proceso para contratar cualquiera de <br />
+                    nuestros productos es fácil y sencillo.
+                  </p>
+                </Fade>
+              )}
             </li>
             <li>
-              <p className="services__title">
+              <p
+                className="services__title "
+                onMouseOver={handleMouseOver2}
+                onMouseOut={handleMouseOut2}
+              >
                 Productos a la medida
               </p>
-              <p className="services-hover">
-              Productos que se adaptan a <br />
-              tus necesidades.
-              </p>
+                {isHovering2 && (
+                  <p className="fs-2 text-center">
+                    Productos que se adaptan a <br />
+                    tus necesidades.
+                  </p>
+                )}
             </li>
             <li>
-              <p className="services__title">
+              <p
+                className="services__title"
+                onMouseOver={handleMouseOver3}
+                onMouseOut={handleMouseOut3}
+              >
                 Atención personalizada
               </p>
-              <p className="services-hover">
-              Siempre podrás hablar con<br />
-              alguien para resolver tus dudas.
-              </p>
+              {isHovering3 && (
+                <p className="fs-2 text-center">
+                  Siempre podrás hablar con
+                  <br />
+                  alguien para resolver tus dudas.
+                </p>
+              )}
             </li>
             <li>
-              <p className="services__title">
-                innovación 
+              <p
+                className="services__title"
+                onMouseOver={handleMouseOver4}
+                onMouseOut={handleMouseOut4}
+              >
+                innovación
               </p>
-              <p className="services-hover">
-              Nos adaptamos a nuevas<br />
-              formas de comunicación para<br />
-              ofrecer un servicio eficiente.
-              </p>
+              {isHovering4 && (
+                <p className="fs-2 text-center ">
+                  Nos adaptamos a nuevas
+                  <br />
+                  formas de comunicación para
+                  <br />
+                  ofrecer un servicio eficiente.
+                </p>
+              )}
             </li>
           </ul>
         </div>
@@ -72,7 +138,8 @@ export const NewCharacteristics = () => {
                   Productos a la medida
                 </p>
                 <p className="card-title fw-normal lh-sm text-primary fs-2 mt-5 align-self-center">
-                  Productos que se adaptan a<br />tus necesidedes.
+                  Productos que se adaptan a<br />
+                  tus necesidedes.
                 </p>
               </div>
             </div>
@@ -87,7 +154,8 @@ export const NewCharacteristics = () => {
                   Procesos eficientes
                 </p>
                 <p className="card-title fw-normal lh-sm text-primary fs-2 mt-5">
-                  El proceso para contratar cualquiera de <br />nuestros productos es fácil y sencillo.
+                  El proceso para contratar cualquiera de <br />
+                  nuestros productos es fácil y sencillo.
                 </p>
               </div>
             </div>
@@ -102,7 +170,8 @@ export const NewCharacteristics = () => {
                   Atención personalizada
                 </p>
                 <p className="card-title fw-normal lh-sm text-primary fs-2 mt-5">
-                  Siempre podrás hablar con alguien <br />para resolver tus dudas.
+                  Siempre podrás hablar con alguien <br />
+                  para resolver tus dudas.
                 </p>
               </div>
             </div>
@@ -117,7 +186,11 @@ export const NewCharacteristics = () => {
                   Innovación
                 </p>
                 <p className="card-title fw-normal lh-sm text-primary fs-2 mt-5">
-                  Nos adaptamos a nuevas formas de<br />comunicación, para ofrecer un servicio<br />eficiente.
+                  Nos adaptamos a nuevas formas de
+                  <br />
+                  comunicación, para ofrecer un servicio
+                  <br />
+                  eficiente.
                 </p>
               </div>
             </div>
@@ -125,7 +198,11 @@ export const NewCharacteristics = () => {
         </div>
       </div>
       <div className="d-flex justify-content-center w-100 d-none d-md-block">
-        <img src="https://elementos-red-enlace.s3.amazonaws.com/Landing/Recurso+2.png" alt="" className="green_stain_bg"/>
+        <img
+          src="https://elementos-red-enlace.s3.amazonaws.com/Landing/Recurso+2.png"
+          alt=""
+          className="green_stain_bg"
+        />
       </div>
     </CharacteristicsSection>
   );
@@ -152,14 +229,14 @@ const CharacteristicsSection = styled.section`
   }
 
   .services__title {
-    font-size: 2rem;
-    transition: visibility 2s, opacity 0.5s linear, font-weight .15s;
+    font-size: 2.5rem;
+    transition: visibility 2s, opacity 0.5s linear, font-weight 0.15s;
 
     &:hover {
       font-weight: 700;
     }
 
-    &:hover>.services-hover {
+    &:hover > .services-hover {
       opacity: 1;
     }
   }
@@ -193,14 +270,14 @@ const CharacteristicsSection = styled.section`
 
   .text-primary,
   .border-primary {
-    color: #FFFFFF !important;
-    border-color:  #78BC9E !important;
+    color: #ffffff !important;
+    border-color: #78bc9e !important;
   }
 
   .card {
     width: 28rem;
     height: 40rem;
-    background-color: #78BC9E;
+    background-color: #78bc9e;
 
     .icon-height {
       height: 15rem;
@@ -220,7 +297,7 @@ const CharacteristicsSection = styled.section`
     }
 
     &:hover {
-      background-color: #FFFFFF;
+      background-color: #ffffff;
     }
   }
 
