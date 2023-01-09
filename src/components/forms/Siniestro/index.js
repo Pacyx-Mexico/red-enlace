0b164e6ca66e652a93dfc089f9584a437e58aba4
Doniben <doniben@esperanto.co>
2023-01-09 00:50:57 -0500

    Siniestro exports

---
+++ src/components/forms/Siniestro/index.js
@@ -0,0 +1,7 @@
+export * from "./MainSiniestroAuto";
+export * from "./MainSiniestroGM";
+export * from "./Step1SiniestroA";
+export * from "./Step2SiniestroA";
+export * from './Step3SiniestroA';
+export * from './Step4SiniestroA';
+export * from './FinalSiniestroPage';
