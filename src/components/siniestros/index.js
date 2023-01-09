1462bc0e90ce472b9567bf9065da7f2a50572c48
Doniben <doniben@esperanto.co>
2023-01-09 00:52:23 -0500

    Update siniestro export components

--- src/components/siniestros/index.js
+++ src/components/siniestros/index.js
@@ -3,3 +3,10 @@ export * from './SiniestroSteps'
 export * from './SelectSiniestro'
 export * from './SiniestroGM'
 export * from './SiniestroAuto'
+export * from "./MainSiniestroAuto";
+export * from "./MainSiniestroGM";
+export * from "./Step1SiniestroA";
+export * from "./Step2SiniestroA";
+export * from './Step3SiniestroA';
+export * from './Step4SiniestroA';
+export * from './FinalSiniestroPage';
