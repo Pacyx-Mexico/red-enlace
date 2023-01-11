6274259b1972fcee19b4f6a8104c612296b2e196
Doniben <doniben@esperanto.co>
2023-01-11 13:43:32 -0500

    desktop styles on step 3

--- src/components/siniestros/Step3SiniestroA.jsx
+++ src/components/siniestros/Step3SiniestroA.jsx
@@ -25,19 +25,29 @@ export const Step3SiniestroA = ({ nextStep, handleFormData, values }) => {
     <>
       <SiniestroAContainer>
         <div>
+          <div className="d-flex justify-content-center flex-column">
+            <h1 className="fst-italic fs-1 fw-bold mt-5">PASO 3/4</h1>
+            <p className="text-center fs-2 fw-normal mt-5">
+              Proporciona el nombre y fecha del siniestro.
+            </p>
+          </div>
+
           <Card style={{ marginTop: 100 }}>
-            <Card.Body>
-              <Form onSubmit={submitFormData}>
+            <Card.Body className="container mx-auto d-flex flex-column justify-content-center align-items-center">
+              <Form
+                onSubmit={submitFormData}
+                className="d-flex justify-content-center align-items-center gap-5 w-100 gx-5"
+              >
                 <Form.Group className="mb-3">
-                  <Form.Label>Nombre del siniestro</Form.Label>
                   <Form.Control
-                  /* Cambiar por dropDown */
+                    /* Cambiar por dropDown */
                     style={{ border: error ? "2px solid red" : "" }}
                     name="nombreSiniestro"
                     defaultValue={values.nombreSiniestro}
                     type="text"
-                    placeholder="nombreSiniestro"
+                    placeholder="nombre del siniestro/ padecimiento"
                     onChange={handleFormData("nombreSiniestro")}
+                    className="input-text"
                   />
                   {error ? (
                     <Form.Text style={{ color: "red" }}>
@@ -48,15 +58,15 @@ export const Step3SiniestroA = ({ nextStep, handleFormData, values }) => {
                   )}
                 </Form.Group>
                 <Form.Group className="mb-3">
-                  <Form.Label>Fecha</Form.Label>
                   <Form.Control
-                  /* Cambiar por dropDown */
+                    /* Cambiar por dropDown */
                     style={{ border: error ? "2px solid red" : "" }}
                     name="date"
                     defaultValue={values.date}
                     type="text"
                     placeholder="Fecha"
                     onChange={handleFormData("date")}
+                    className="input-text"
                   />
                   {error ? (
                     <Form.Text style={{ color: "red" }}>
@@ -66,8 +76,9 @@ export const Step3SiniestroA = ({ nextStep, handleFormData, values }) => {
                     ""
                   )}
                 </Form.Group>
-                <Button variant="primary" type="submit" text="Siguiente">
-                </Button>
+                <div className="d-flex justify-content-center mt-5 pt-5">
+                  <Button variant="primary" type="submit" text="Siguiente" />
+                </div>
               </Form>
             </Card.Body>
           </Card>
@@ -80,4 +91,28 @@ export const Step3SiniestroA = ({ nextStep, handleFormData, values }) => {
 const SiniestroAContainer = styled.section`
   height: 50rem;
   margin-top: 15rem;
+
+  @media (min-width: 992px) {
+    height: 90vh;
+    .card {
+      border: none;
+    }
+
+    .card-body {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .input-text {
+      border-radius: 1rem;
+      background: #f2f5fc;
+      border: none;
+      width: 60rem;
+      font-size: 12px;
+
+      &::placeholder {
+        color: #25282c;
+      }
+    }
+  }
 `;
