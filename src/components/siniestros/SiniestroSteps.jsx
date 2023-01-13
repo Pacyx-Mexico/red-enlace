3bb9271dfa8de891eef854fceafe97a10a535d18
Doniben <doniben@esperanto.co>
2023-01-13 12:45:44 -0500

    Siniestro steps mobile styles

--- src/components/siniestros/SiniestroSteps.jsx
+++ src/components/siniestros/SiniestroSteps.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { MedicineIcon, MedicalExpensesIcon, CarIcon } from "../../assets/icons";
 import { NavLink, Link } from "react-router-dom";
 import Button from "../Button";
+import { IoIosArrowDown } from "react-icons/io";
 
 export const SiniestroSteps = () => {
   const scrollTop = () => {
@@ -16,8 +17,8 @@ export const SiniestroSteps = () => {
           <span className="text-black fw-bold"> 3 sencillos pasos</span>
         </h3>
       </div>
-      <div className="services-title-mb container-fluid text-center d-lg-none">
-        <h4 className="fw-semi-bold text-dar mb-5 pb-5">
+      <div className="container-fluid text-center d-lg-none">
+        <h4 className="fw-semi-bold services-title-mb fst-italic text-dark mb-5 pb-5">
           Conoce nuestros productos
         </h4>
       </div>
@@ -75,55 +76,83 @@ export const SiniestroSteps = () => {
 
       <div className="container-fluid py-3 position-relative d-lg-none">
         <div className="flex-column px-5 mx-5 mr-5 ml-5 d-flex align-items-center justify-content-center gap-5">
-          <div className="col-auto mb-5 pb-5 w-75">
-            <div className="card border border-4 border-first rounded-5 text-center opacity-75 position-relative card-hover_first d-flex align-items-center justify-content-center mx-auto shadow-lg">
-              <div className="icon-width position-absolute d-flex align-items-center justify-content-center">
-                <div className="icon-width d-flex align-items-center justify-content-center">
-                  <h2 className="number-fs">1</h2>
+          <div className="d-flex flex-column justify-content-center w-100">
+            <div className="d-flex align-items-center justify-content-center gap-5">
+              <div className="d-flex flex-row align-items-center gap-5 me-4 pe-3">
+                <div className="d-flex flex-column jsutify-content-center align-items-center gap-5">
+                  <div className="card border-0 rounded-5 text-center position-relative card-hover_first d-flex align-items-center justify-content-center mx-auto">
+                    <div className="d-flex align-items-center justify-content-center">
+                      <div className="icon-width d-flex align-items-center justify-content-center">
+                        <h2 className="number-fs fw-normal">1</h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="arrow d-flex justify-content-center my-5 opacity-75">
+                    <IoIosArrowDown />
+                  </div>
+                </div>
+                <div className="mb-5 pb-5 d-flex justify-content-center">
+                  <p className="card-title fw-normal lh-sm w-100 mb-5 pb-5">
+                    Selecciona el producto
+                  </p>
                 </div>
               </div>
             </div>
-            <div className="mt-5 d-flex justify-content-center">
-              <p className="card-title fw-normal lh-sm text-center fs-2 w-100">
-                Selecciona el producto
-              </p>
-            </div>
           </div>
-          <div className="col-auto mb-5 pb-5 w-75">
-            <div className="card border border-4 border-second rounded-5 text-center opacity-75 position-relative card-hover_second d-flex align-items-center justify-content-center mx-auto shadow-lg">
-              <div className="icon-width position-absolute d-flex align-items-center justify-content-center">
-                <div className="icon-width d-flex align-items-center justify-content-center">
-                  <h2 className="number-fs">2</h2>
+
+          <div className="d-flex flex-column justify-content-center w-100">
+            <div className="d-flex align-items-center justify-content-center gap-5">
+              <div className="d-flex flex-row align-items-center gap-5 me-4">
+                <div className="d-flex flex-column jsutify-content-center align-items-center gap-5">
+                  <div className="card border-0 rounded-5 text-center position-relative card-hover_first d-flex align-items-center justify-content-center mx-auto">
+                    <div className="d-flex align-items-center justify-content-center">
+                      <div className="icon-width d-flex align-items-center justify-content-center">
+                        <h2 className="number-fs fw-normal">2</h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="arrow d-flex justify-content-center my-5 opacity-75">
+                    <IoIosArrowDown />
+                  </div>
+                </div>
+                <div className="mb-5 pb-5 d-flex justify-content-center">
+                  <p className="card-title fw-normal lh-sm w-100 mb-5 pb-5">
+                    Descarga y requisita los <br />
+                    formatos correspondientes, <br />
+                    no olvides firmarlos
+                  </p>
                 </div>
               </div>
             </div>
-            <div className="mt-5 d-flex justify-content-center">
-              <p className="card-title fw-normal lh-sm text-center fs-2 w-100">
-                Descarga y requisita los <br />
-                formatos correspondientes, <br />
-                no olvides firmarlos
-              </p>
-            </div>
           </div>
-          <div className="mb-5 pb-5 w-75">
+
+          <div>
             <NavLink
               onClick={scrollTop}
               to="/deducible-gastos-medicos"
-              className="w-100 text-decoration-none"
+              className="w-100 text-dark text-decoration-none"
             >
-              <div className="card border border-4 border-third rounded-5 text-center opacity-75 position-relative card-hover_third d-flex align-items-center justify-content-center mx-auto shadow-lg">
-                <div className="icon-width position-absolute d-flex align-items-center justify-content-center">
-                  <div className="icon-width d-flex align-items-center justify-content-center">
-                    <h2 className="number-fs">3</h2>
+              <div className="d-flex flex-column justify-content-center w-100">
+                <div className="d-flex align-items-center justify-content-center gap-5">
+                  <div className="d-flex flex-row align-items-center gap-5 me-4">
+                    <div className="d-flex flex-column jsutify-content-center align-items-center gap-5">
+                      <div className="card border-0 rounded-5 text-center position-relative card-hover_first d-flex align-items-center justify-content-center mx-auto">
+                        <div className="d-flex align-items-center justify-content-center">
+                          <div className="icon-width d-flex align-items-center justify-content-center">
+                            <h2 className="number-fs fw-normal">3</h2>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                      <p className="card-title fw-normal lh-sm w-100">
+                        Completa tu información y
+                        adjunta la documentación <br />
+                        solicitada
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="mt-5 d-flex justify-content-center">
-                <p className="card-title fw-normal lh-sm text-center fs-2 w-100">
-                  Completa tu información y <br />
-                  adjunta la documentación <br />
-                  solicitada
-                </p>
               </div>
             </NavLink>
           </div>
@@ -155,10 +184,11 @@ const ServicesContainer = styled.section`
   .card-title {
     margin-bottom: 0rem;
     color: #000000;
+    font-size: 2.5rem;
   }
 
   .number-fs {
-    font-size: 30rem;
+    font-size: 19rem;
   }
 
   .text-,
@@ -189,8 +219,9 @@ const ServicesContainer = styled.section`
   }
 
   .card {
-    width: 41rem;
-    height: 41rem;
+    width: 30rem;
+    height: 30rem;
+    background: #e8ebfa;
   }
 
   .card_img-width_first {
@@ -213,6 +244,15 @@ const ServicesContainer = styled.section`
 
   .rounded-5 {
     border-radius: 5.5rem !important;
+  }
+
+  .arrow {
+    width: 40%;
+  }
+
+  .arrow svg {
+    width: auto;
+    height: 5rem;
   }
 
   .btn-first {
@@ -271,6 +311,10 @@ const ServicesContainer = styled.section`
     padding-bottom: 20rem;
   }
 
+  .services-title-mb {
+    font-size: 4.5rem;
+  }
+
   .gap-10 {
     gap: 10rem;
   }
@@ -297,11 +341,6 @@ const ServicesContainer = styled.section`
   @media (min-width: 768px) {
     padding-top: 20rem;
     padding-bottom: 20rem;
-
-    .services-title-mb h3 {
-      padding: 0 3rem 15rem;
-      text-align: start;
-    }
 
     .card {
       width: 50rem;
